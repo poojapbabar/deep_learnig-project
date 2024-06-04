2584bc3f7254f27b3f0afc1df1c69577768bb040
pooja babar <babarpooja2002@gmail.com>
2024-06-04 09:37:39 +0530

    requirement

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
-from setuptools import find_packages,setup
+from setuptools import setup, find_packages
 from typing import List
 
-HYPEN_E_DOT='-e .'
+HYPEN_E_DOT = '-e .'
 
-def get_requirements(file_path:str)->List[str]:
-    requirements=[]
+def get_requirements(file_path: str) -> List[str]:
+    requirements = []
     with open(file_path) as file_obj:
-        requirements=file_obj.readlines()
-        requirements=[req.replace("\n","") for req in requirements]
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n", "") for req in requirements]
 
         if HYPEN_E_DOT in requirements:
             requirements.remove(HYPEN_E_DOT)
@@ -16,13 +16,10 @@ def get_requirements(file_path:str)->List[str]:
 
 
 setup(
-
-name="Xray",
-version="0.0.1",
-author="pooja babar",
-author_email="babarpooja2002@gmail.com",
-install_requires=get_requirements(r"REQUIREMENTS.TXT"),
-package=find_packages()
-
+    name="Xray",
+    version="0.0.1",
+    author="pooja babar",
+    author_email="babarpooja2002@gmail.com",
+    install_requires=get_requirements(r"D:\Deep_learning\REQUIREMENTS.TXT"),
+    packages=find_packages(),
 )
-
